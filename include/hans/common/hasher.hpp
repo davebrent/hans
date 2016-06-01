2902647d298afcf6dbe8fb7fe68cea46932d22e1
Dave Poulter <dapoulter@gmail.com>
2016-06-01 11:00:47 +0100

    Separate hash function from string manager

---
+++ include/hans/common/hasher.hpp
@@ -0,0 +1,14 @@
+#ifndef HANS_COMMON_HASHER_H_
+#define HANS_COMMON_HASHER_H_
+
+#include "hans/common/types.hpp"
+
+namespace hans {
+namespace common {
+
+hans_hash hasher(const char* str);
+
+} // namespace common
+} // namespace hans
+
+#endif // HANS_COMMON_HASHER_H_
--- lib/common/StringManager.cpp
+++ lib/common/StringManager.cpp
@@ -2,54 +2,9 @@
 #include <algorithm>
 #include <cassert>
 #include <cstring>
+#include "hans/common/hasher.hpp"
 
 using namespace hans;
-
-static uint64_t murmur_hash_64(const void* key, uint32_t len, uint64_t seed) {
-  const uint64_t m = 0xc6a4a7935bd1e995ULL;
-  const uint32_t r = 47;
-
-  uint64_t h = seed ^ (len * m);
-
-  const uint64_t* data = static_cast<const uint64_t*>(key);
-  const uint64_t* end = data + (len / 8);
-
-  while (data != end) {
-    uint64_t k = *data++;
-
-    k *= m;
-    k ^= k >> r;
-    k *= m;
-
-    h ^= k;
-    h *= m;
-  }
-
-  const unsigned char* data2 = reinterpret_cast<const unsigned char*>(data);
-
-  switch (len & 7) {
-  case 7:
-    h ^= uint64_t(data2[6]) << 48;
-  case 6:
-    h ^= uint64_t(data2[5]) << 40;
-  case 5:
-    h ^= uint64_t(data2[4]) << 32;
-  case 4:
-    h ^= uint64_t(data2[3]) << 24;
-  case 3:
-    h ^= uint64_t(data2[2]) << 16;
-  case 2:
-    h ^= uint64_t(data2[1]) << 8;
-  case 1:
-    h ^= uint64_t(data2[0]);
-    h *= m;
-  };
-
-  h ^= h >> r;
-  h *= m;
-  h ^= h >> r;
-  return h;
-}
 
 common::StringManager::StringManager(size_t size) : m_allocator(size) {
 }
@@ -57,7 +12,7 @@ common::StringManager::StringManager(size_t size) : m_allocator(size) {
 hans_hash common::StringManager::intern(const char* string) {
   // Check if we have seen the string before
   size_t len = strlen(string);
-  hans_hash hash = murmur_hash_64(string, len * sizeof(char), 0);
+  hans_hash hash = common::hasher(string);
 
   auto end = m_hashes.end();
   auto it = std::find(m_hashes.begin(), end, hash);
