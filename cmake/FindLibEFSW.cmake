361d7d17f513ecb551ea77b5a237f3b7c398b11b
Dave Poulter <hello@davepoulter.net>
2017-03-03 21:29:25 +0000

    Fix finding libefsw

--- cmake/FindLibEFSW.cmake
+++ cmake/FindLibEFSW.cmake
@@ -1,18 +1,12 @@
-find_path(LIBEFSW_INCLUDE_DIRS "efsw.hpp"
-  PATHS
-    ${CMAKE_SOURCE_DIR}/include
-    /usr/local/include/efsw
-    /usr/include/efsw)
+find_path(LIBEFSW_INCLUDE_DIRS NAMES efsw/efsw.hpp)
+find_library(LIBEFSW_LIBRARIES NAMES efsw)
 
-find_library(LIBEFSW_LIBRARY_DIRS libefsw libefsw.so libefsw.dylib
-  PATHS
-    ${CMAKE_SOURCE_DIR}/lib
-    /usr/local/lib
-    /usr/lib)
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(LIBEFSW REQUIRED_VARS LIBEFSW_INCLUDE_DIRS LIBEFSW_LIBRARIES)
 
-SET(LIBEFSW_LIBRARIES ${LIBEFSW_LIBRARY_DIRS})
+mark_as_advanced(LIBEFSW_INCLUDE_DIRS LIBEFSW_LIBRARIES)
 
 set(LIBEFSW_FOUND FALSE)
-if(LIBEFSW_LIBRARY_DIRS AND LIBEFSW_INCLUDE_DIRS)
+if(LIBEFSW_LIBRARIES AND LIBEFSW_INCLUDE_DIRS)
   set(LIBEFSW_FOUND TRUE)
-endif(LIBEFSW_LIBRARY_DIRS AND LIBEFSW_INCLUDE_DIRS)
+endif(LIBEFSW_LIBRARIES AND LIBEFSW_INCLUDE_DIRS)
